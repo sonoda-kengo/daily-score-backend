e7b78289aad95b7b3579f4c596c1d8af2138641e
sonoda-kengo <wsho.magn07@gmail.com>
2023-06-22 19:35:13 +0900

    use swagger UI

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "dj_rest_auth",
     "corsheaders",
+    "drf_yasg",
     # app
     "apiv1",
     "task",
--- config/urls.py
+++ config/urls.py
@@ -16,5 +16,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
-urlpatterns = [path("admin/", admin.site.urls), path("apiv1/", include("apiv1.urls"))]
+schema_view = get_schema_view(
+    openapi.Info(
+        title="TASK API",
+        default_version="v1",
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("apiv1/", include("apiv1.urls")),
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+]
